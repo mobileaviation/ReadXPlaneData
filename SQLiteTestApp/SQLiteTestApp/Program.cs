d79a1ff19a8dbc9465cea2e0c4428d0e528c860d
rob.verhoef <rob.verhoef@united4all.nl>
2015-03-17 13:08:12 +0100

    Test app with spatialite 3.0.1. DLL's added1

--- SQLiteTestApp/SQLiteTestApp/Program.cs
+++ SQLiteTestApp/SQLiteTestApp/Program.cs
@@ -17,28 +17,26 @@ namespace SQLiteTestApp
             m_dbConnection.Open();
 
             SQLiteCommand cmd;
-<<<<<<< HEAD
+
             //cmd = new SQLiteCommand(m_dbConnection);
             //cmd.CommandText = "create table highscores (name varchar(20), score int)";
             //cmd.ExecuteNonQuery();
-=======
->>>>>>> 5c849b4ceb7412cc24b068b908cf5d928088b3ea
 
             cmd = new SQLiteCommand(m_dbConnection);
             cmd.CommandText = "SELECT load_extension('libspatialite-2.dll')";
             cmd.ExecuteNonQuery();
 
-            //cmd = new SQLiteCommand(m_dbConnection);
-            //cmd.CommandText = "SELECT InitSpatialMetaData();";
-            //cmd.ExecuteNonQuery();
+            cmd = new SQLiteCommand(m_dbConnection);
+            cmd.CommandText = "SELECT InitSpatialMetaData();";
+            cmd.ExecuteNonQuery();
 
-            //cmd = new SQLiteCommand(m_dbConnection);
-            //cmd.CommandText = "CREATE TABLE	test_geom (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,measured_value DOUBLE NOT NULL);";
-            //cmd.ExecuteNonQuery();
+            cmd = new SQLiteCommand(m_dbConnection);
+            cmd.CommandText = "CREATE TABLE	test_geom (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,measured_value DOUBLE NOT NULL);";
+            cmd.ExecuteNonQuery();
 
-            //cmd = new SQLiteCommand(m_dbConnection);
-            //cmd.CommandText = "SELECT AddGeometryColumn('test_geom', 'the_geom', 4326, 'POINT', 2);";
-            //cmd.ExecuteNonQuery();
+            cmd = new SQLiteCommand(m_dbConnection);
+            cmd.CommandText = "SELECT AddGeometryColumn('test_geom', 'the_geom', 4326, 'POINT', 2);";
+            cmd.ExecuteNonQuery();
 
             cmd = new SQLiteCommand(m_dbConnection);
             cmd.CommandText = "INSERT INTO test_geom(id, name, measured_value, the_geom) VALUES (NULL, 'first point', 1.23456, GeomFromText('POINT(1.01 2.02)', 4326));";
